776c13f15166aa0cd8d61c48ad55f398ec9368ef
nick <wrl.snk@gmail.com>
2022-05-27 01:40:51 +0300

    log, detail plots

--- csv handler.py
+++ csv handler.py
@@ -1,33 +1,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import fitter as f
+import datetime
+
+
+# presets
+plt.style.use('seaborn')
 
 # whole csv data
 csv = u'data\grand dataset.csv'
 df = pd.read_csv(csv, header=0, index_col=False, names = ['Organization', 'Project', 'Issue','Status', 'Assignee', 'Created', 'Resolved', 'SP', 'Time Spent'])
 # sampling parameters
-trainingAmount = .70
-project = 'MXNET'
+training_amount = .70
+test_project = 'HUDI'
 
-# for project in df['Project'].unique():
+def write_density(project, density):
+    f = open('plots/densities.txt', 'a')
+    f.write(project + ' (' + datetime.datetime.now().strftime('%d.%m %H:%M') + '):\n' + str(density))
 
 def collect_data(project):
     # actual data
     data = pd.DataFrame(df[df['Project'] == project], columns= ['Issue', 'SP', 'Time Spent'])
     data.set_index('Issue', inplace=True)
+
+    # when we have 0 SP issues and don't wanna infinity
+    if (data['SP'] ==0).any():
+        data['SP']= data['SP'] + 1
+
+    # adding calculated column of 1 SP cost
     data['SP Cost'] = data['Time Spent'] / data['SP'] 
+
     # splitting to training and validation dataset
-    training = data.sample(frac=trainingAmount)
+    training = data.sample(frac=training_amount)
     validation = data.drop(training.index)
     print('There are ' + str(training.shape[0]) + ' datapoints in training dataset')
 
-    fitter = f.Fitter(training['SP Cost'], distributions = f.get_common_distributions())
+    # checking density functions 
+    fitter = f.Fitter(training['SP Cost'], distributions = f.get_distributions())
     fitter.fit()
-    print(fitter.get_best(method = 'sumsquare_error'))
+    write_density(project, fitter.get_best(method = 'sumsquare_error'))
     fitter.summary()
-
-def show_plot(data, project):
     plt.title(label = (str(data.shape[0]) + ' datapoints of ' + project))
+    return training
+
+def show_plot():
     plt.show()
 
 def save_plot(project):
@@ -35,3 +51,8 @@ def save_plot(project):
 
 def count_issues():
     print(df.groupby('Project')['Issue'].nunique())
+
+for project in df['Project'].unique():
+    print(project)
+    collect_data(project)
+    save_plot(project)
