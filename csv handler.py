d94d02a4eaeaead08a39a9d407f54a8ea89a3a60
nick <wrl.snk@gmail.com>
2022-05-28 03:20:26 +0300

    better logging

--- csv handler.py
+++ csv handler.py
@@ -1,3 +1,4 @@
+from statistics import median
 from wsgiref import validate
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,7 +6,7 @@ import fitter as f
 import numpy as np
 import scipy.stats as sc
 import datetime
-
+import math
 
 # presets
 plt.style.use('seaborn')
@@ -15,9 +16,14 @@ csv = u'data\grand dataset.csv'
 df = pd.read_csv(csv, header=0, index_col=False, names = ['Organization', 'Project', 'Issue','Status', 'Assignee', 'Created', 'Resolved', 'SP', 'Time Spent'])
 # model parameters
 training_amount = .70
-test_project = 'HUDI'
+low = .05
+high = .95
 repeats = 10
 seed = 123
+test_project = 'HUDI'
+
+def count_issues():
+    print(df.groupby('Project')['Issue'].nunique())
 
 # Remove unnecessary data, create training and validation datasets
 def collect_data(project):
@@ -41,21 +47,6 @@ def collect_data(project):
 
     return training, validation
 
-# Write density info to txt file
-def write_density(project, density):
-    f = open('data/densities.txt', 'a')
-    f.write(project + ' (' + datetime.datetime.now().strftime('%d.%m %H:%M') + '):\n' + str(density))
-
-# Call Fitter to find best fitting density distributions
-def analyse_density(data, project, all_dist, plot):
-    # checking density functions 
-    fitter = f.Fitter(data['SP Cost'], distributions = f.get_distributions() if all_dist else f.get_common_distributions())
-    fitter.fit()
-    density = fitter.get_best(method = 'sumsquare_error')
-    write_density(project, density + '\n\n')
-    if plot: fitter.summary()
-    return density
-
 # Draw scatters
 def plot_scatter(data):
     x = data['Time Spent']
@@ -66,6 +57,20 @@ def plot_scatter(data):
     z = np.polyfit(x, y, 1)
     p = np.poly1d(z)
     plt.plot(x, p(x), 'r--')
+
+# Call Fitter to find best fitting density distributions
+def find_distribution(data, project, all_dist, plot):
+    # checking density functions 
+    fitter = f.Fitter(data['SP Cost'], distributions = f.get_distributions() if all_dist else f.get_common_distributions())
+    fitter.fit()
+    density = fitter.get_best(method = 'sumsquare_error')
+    if plot: fitter.summary()
+    return density
+
+# Write density info to txt file
+def write_density(project, density):
+    f = open('data/densities.txt', 'a')
+    f.write(project + ' (' + datetime.datetime.now().strftime('%d.%m %H:%M') + '):\n' + str(density))
 
 def show_plot():
     plt.show()
@@ -82,14 +87,11 @@ def save_plot(project, folder = '', suffix=''):
     plt.savefig(path, bbox_inches='tight')
     plt.close()
 
-def count_issues():
-    print(df.groupby('Project')['Issue'].nunique())
-
-def generating(data, project, dist, repeats = 10000):
+def generate(data, project, dist, repeats = 10000, log = False):
     # Making sure our random stays predictable
     np.random.seed(seed=seed)
     # Creating special dataset to write up
-    mcs = pd.DataFrame(columns=list(data.index.values))
+    mc = pd.DataFrame(columns=list(data.index.values))
 
     # Converting object to string 'sc.dist.rvs(key = value, key2 = ...)'
     dist_name = list(dist.keys())[0]
@@ -100,23 +102,50 @@ def generating(data, project, dist, repeats = 10000):
     dist_string = 'sc.'+dist_name+'.rvs('+dist_params+'size='+str(repeats)+')'
 
     # Generating 
-    for issue in mcs:
+    for issue in mc:
         sp_count = data.loc[issue, 'SP']
-        print(eval(dist_string))
-        mcs[issue] = eval(dist_string)*sp_count
+        mc[issue] = eval(dist_string)*sp_count
+    
+    # Caluculating the sum
+    mc['Sum'] = mc.sum(axis=1)
 
-# Write generated data to file
-def save_mcs(project, data, mcs):
-    f = open('data/generated data.txt', 'a')
-    f.write(project + ', ' + str(data.shape[0]) + ' datapoints (' + 
-    datetime.datetime.now().strftime('%d.%m %H:%M') + '):\n' + mcs.to_string() + '\n\n')
+    if log:
+        f = open('data/generated data.txt', 'a')
+        f.write(project + ', ' + str(mc.shape[1]-1) + ' datapoints (' + datetime.datetime.now().strftime('%d.%m %H:%M') + '):\n' + str(mc) + '\n\n')
+        f.close()
+
+    return mc
+
+# Estimate in terms of low, median and high scores
+def estimate(data : pd.DataFrame):
+    optimistic = data['Sum'].quantile(low)
+    median = data['Sum'].quantile(.5)
+    pessimistic = data['Sum'].quantile(high)
+    return median, pessimistic, optimistic
+
+# Calculate error
+def calc_error(data : pd.DataFrame, m, p, o, log = False):
+    real = data['Time Spent'].sum()
+    e = m-real
+    re = e/real*100
+    in_range = o <= real <= p
+
+    if log:    
+        def r(int): return str(round(int))  # rounded to string
+        f = open('data/generated data.txt', 'a')
+        f.write('Real: ' + r(real) + '\tEst: ' + r(m) + 
+                '\nError: ' + r(e) + '\tRel Error:' + r(re) + '%\t' '★'*math.floor(100/re) + '☆'+ math.floor(re/20)+
+                '\nRange: [' + r(o) + ' — ' + r(p) + ']\t' + '✔️' if in_range else '❌')
+
+    return e, in_range
 
 # Like, do it for all projects
 def iterate(all_dist=True):
     for project in df['Project'].unique():
         print(project)
         training, validation = collect_data(project)
-        analyse_density(training, project, all_dist, plot=True)
+        dens = find_distribution(training, project, all_dist, plot=True)
+        write_density(project, dens)
         save_plot(project)
     
 # Do it for test project
@@ -127,4 +156,6 @@ def test(all_dist=False):
     show_plot()
 
 training, validation = collect_data(test_project)
-generating(validation, test_project, repeats=repeats)
+dist = find_distribution(training, test_project, False, True)
+mc = generate(validation, test_project, dist, repeats=repeats)
+calc_error(validation, *estimate(mc))
